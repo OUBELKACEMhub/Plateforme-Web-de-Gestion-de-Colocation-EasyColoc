30ec2e06e23f1ea4b77d414574dcbb1bbd6ca7b4
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-24 23:37:23 +0000

    Add Laravel Blade component

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,16 @@
+<!-- <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <rect width="48" height="48" rx="12" fill="#FF750F"/>
+    
+    <path d="M14 24V14H34M14 24H30M14 24V34H34" 
+          stroke="white" 
+          stroke-width="3.5" 
+          stroke-linecap="round" 
+          stroke-linejoin="round"/>
+          
+    <path d="M10 16L24 6L38 16" 
+          stroke="white" 
+          stroke-width="3" 
+          stroke-linecap="round" 
+          stroke-linejoin="round" 
+          style="opacity: 0.8;"/>
+</svg> -->
