aa1be5cd9599f7ab1c60e8032cbf220eca34328a
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-26 00:46:20 +0000

    creat class checkBanned to check if an user is banned or not and used this function like middleware

--- app/Http/Middleware/CheckBanned.php
+++ app/Http/Middleware/CheckBanned.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class CheckBanned
 {
     public function handle(Request $request, Closure $next): Response
