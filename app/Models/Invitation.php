07cbbd4f14939b0920d6c870f52269784262c89c
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-26 00:33:55 +0000

    creat model invitation with filiable variable

---
+++ app/Models/Invitation.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Invitation extends Model
+{
+    protected $fillable = [
+        'email',
+        'token',
+        'expires_at',
+        'status',
+        'colocation_id',
+        'sender_id'
+    ];
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+}
