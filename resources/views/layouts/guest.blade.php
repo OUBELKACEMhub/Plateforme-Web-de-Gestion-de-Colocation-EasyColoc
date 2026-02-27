694feaee11de485c74169723e7949a62eec90d6f
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-27 09:59:10 +0000

    implement clean guest layout for login and registration views

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,7 +23,11 @@
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+                @if(isset($slot))
+    {{ $slot }}
+@else
+    @yield('content')
+@endif
             </div>
         </div>
     </body>
