1cda7ff9e7f955d231b6e41f7de33aec9c36d466
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-03-01 23:57:07 +0000

    add invitation management routes for receiving, accepting, and rejecting requests

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\SettlementController;
 
 
 Route::get('/', function () {
@@ -35,27 +37,39 @@ Route::middleware(['auth', 'verified', 'banned'])->group(function () {
         Route::post('/join', [ColocationController::class, 'join'])->name('join');
 
      Route::get('/{colocation}/invite', [ColocationController::class, 'showInviteForm'])->name('invite');
-     Route::post('/colocations/join', [ColocationController::class, 'join'])->name('colocations.join');
+     Route::post('/colocations/rejoinder', [ColocationController::class, 'joinparToken'])->name('rejoindre');
      Route::post('/{colocation}/invite', [ColocationController::class, 'sendInvitation'])->name('invite.send');
 
      Route::delete('/{colocation}/members/{user}', [ColocationController::class, 'removeMember'])->name('members.remove');
-        
+     Route::delete('/colocations/{colocation}', [ColocationController::class, 'destroy'])->name('destroy');  
+     Route::delete('/colocations/{colocation}/leave', [ColocationController::class, 'leave'])->name('leave');
     });
 
-      Route::middleware(['auth'])->group(function () {
-    Route::get('/invitations/received', [InvitationController::class, 'receivedInvitations'])
-        ->name('colocations.received');
+    
 
-    Route::patch('/invitations/{invitation}/accept', [InvitationController::class, 'accept'])->name('invitations.accept');
-        
-    Route::patch('/invitations/{invitation}/reject', [InvitationController::class, 'reject'])
-        ->name('invitations.reject');
+    Route::prefix('invitations')->as('invitations.')->group(function () {
+    Route::get('/received', [InvitationController::class, 'receivedInvitations'])->name('received');
+    Route::patch('/{invitation}/accept', [InvitationController::class, 'accept'])->name('accept');
+    Route::patch('/{invitation}/reject', [InvitationController::class, 'reject'])->name('reject');
 });
 
 Route::middleware(['can:admin-access'])->prefix('admin')->as('admin.')->group(function () {
     Route::get('/users', [AdminController::class, 'users'])->name('users.index');
 Route::patch('/users/{user}/toggle-ban', [AdminController::class, 'toggleBan'])->name('users.toggle-ban');});
 
+
+
+
+
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+   
+   
+   Route::get('/settlements', [SettlementController::class, 'index'])->name('settlements.index');
+   Route::patch('/settlements/{settlement}/pay', [SettlementController::class, 'pay'])->name('settlements.pay');
 });
 
 require __DIR__.'/auth.php';
