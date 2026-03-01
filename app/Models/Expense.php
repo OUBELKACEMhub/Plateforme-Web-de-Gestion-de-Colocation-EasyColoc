b712c13c04554e5485e381eef4089c13a7f5bbb0
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-03-01 23:44:10 +0000

    update relations with user and colocations and categories tables

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -8,6 +8,6 @@ class Expense extends Model
     protected $fillable = ['title', 'amount', 'date', 'category_id', 'payer_id', 'colocation_id'];
 
     public function payer() { return $this->belongsTo(User::class, 'payer_id'); }
-    public function category() { return $this->belongsTo(Category::class); }
+    public function category() { return $this->belongsTo(Categorie::class); }
     public function colocation() { return $this->belongsTo(Colocation::class); }
 }
