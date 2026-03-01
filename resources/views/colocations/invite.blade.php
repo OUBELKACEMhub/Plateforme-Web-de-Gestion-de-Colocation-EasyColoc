aeb6682d56153c336001f866e69a3f5d71401171
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-03-01 23:49:56 +0000

    implement received invitations view with dynamic data binding

--- resources/views/colocations/invite.blade.php
+++ resources/views/colocations/invite.blade.php
@@ -1,10 +1,8 @@
-@php
-    $layout = Auth::user()->role === 'admin' ? 'layouts.admin' : 'layouts.app';
-@endphp
-
-@extends($layout)
+@extends('layouts.app')
 
 @section('content')
+
+
     <div class="{{ Auth::user()->role !== 'admin' ? 'py-12 px-4 max-w-2xl mx-auto' : 'max-w-2xl mx-auto py-6' }}">
         
         <x-invite-member-form :colocation="$colocation" />
