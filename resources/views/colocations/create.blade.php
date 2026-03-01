d9e3bf1be743a692bcea4e3cadbdad17fea29efc
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-03-01 23:52:39 +0000

    implement creat invitation  form

--- resources/views/colocations/create.blade.php
+++ resources/views/colocations/create.blade.php
@@ -1,16 +1,20 @@
-@php
-    $layout = Auth::user()->role === 'admin' ? 'layouts.admin' : 'layouts.app';
-@endphp
-
-@extends($layout)
+@extends('layouts.app')
 
 @section('content')
-
-    <div class="{{ Auth::user()->role !== 'admin' ? 'py-12 px-4 max-w-7xl mx-auto' : 'py-6' }}">
+    <div class="{{ Auth::user()->is_global_admin ? 'sm:ml-72 py-10 px-6' : 'py-12 px-4 max-w-7xl mx-auto' }}">
         
-        <div class="max-w-lg mx-auto">
-            {{-- L-Component dial l-form li sawbna --}}
-            <x-create-coloc-form />
+        <div class="max-w-xl mx-auto">
+            {{-- Header sghir bach l-admin y-3ref fin rah --}}
+            <div class="mb-8 text-center">
+                <h1 class="text-3xl font-black text-white italic uppercase tracking-tighter">
+                    Lancer un <span class="text-[#FF750F]">Espace</span>
+                </h1>
+                <p class="text-slate-500 text-sm italic mt-2">Créez votre colocation et commencez à gérer vos dépenses.</p>
+            </div>
+
+            <div class="bg-[#151515] p-8 rounded-[2.5rem] border border-white/5 shadow-2xl">
+                <x-create-coloc-form />
+            </div>
         </div>
 
     </div>
